a590faf45aa64d078b3fa5b3bf6b5e8d5c0c5c42
Asvarox93 <asvarox93@gmail.com>
2017-03-31 21:06:41 +0200

    Wprowadzenie poprawnych zdjęc i tekstów w slajderze

--- CRM/src/app/main-page/mainPageService.service.ts
+++ CRM/src/app/main-page/mainPageService.service.ts
@@ -15,10 +15,12 @@ export interface SliderData {
 export class MainPageServiceService {
 
 sliderData:SliderData = {
-  images: ['http://lorempixel.com/900/500?r=1' , 'http://lorempixel.com/900/500?r=2', 'http://lorempixel.com/900/500?r=3'],
-  text: ['Testowa wiadomosc' , 'Ta juz nie jest testowa:)', 'A jak tutaj dojde to cud.'],
+  images: ['../../../assets/mainPage/sliderImg3.jpg' , '../../../assets/mainPage/sliderImg2.jpg', '../../../assets/mainPage/sliderImg1.jpg'],
+  text: ['Sprawna organizacja, prosta obsługa i mnóstwo zadowolonych klientów, polecających nasze usługi. Cenimy sobie zdanie naszych starch jak i nowych osób korzystających z naszych usług...',
+   'Szeroki zakres usług i pomoc to dwa aspekty, które możesz oczekiwać od naszej firmy. Sprawny kontakt i szybkie odpowiedzi na zadanie pytania, czy problemy sprawia, że nasi klienci są zadowoleni...',
+   'Nasza firma, oferuje szeroki zakres usług ułatwiających pracę i zarządzanie klientami. Dzięki Bandalight możesz w prosty sposób zarządzać pracownikami czy też płacić faktury VAT.'],
   slideNumbers: 3,
-  slideInterval: 1500
+  slideInterval: 6500
 };
 
 getSliderImages(){
