2f2958b5fe819580aa0de810944a0d3585dcd561
Asvarox93 <asvarox93@gmail.com>
2017-04-01 10:52:16 +0200

    Wykonana sekcja z formularzem kontaktowym na str głównej

--- CRM/src/app/main-page/mainPageService.service.ts
+++ CRM/src/app/main-page/mainPageService.service.ts
@@ -18,7 +18,7 @@ sliderData:SliderData = {
   images: ['../../../assets/mainPage/sliderImg3.jpg' , '../../../assets/mainPage/sliderImg2.jpg', '../../../assets/mainPage/sliderImg1.jpg'],
   text: ['Sprawna organizacja, prosta obsługa i mnóstwo zadowolonych klientów, polecających nasze usługi. Cenimy sobie zdanie naszych starch jak i nowych osób korzystających z naszych usług...',
    'Szeroki zakres usług i pomoc to dwa aspekty, które możesz oczekiwać od naszej firmy. Sprawny kontakt i szybkie odpowiedzi na zadanie pytania, czy problemy sprawia, że nasi klienci są zadowoleni...',
-   'Nasza firma, oferuje szeroki zakres usług ułatwiających pracę i zarządzanie klientami. Dzięki Bandalight możesz w prosty sposób zarządzać pracownikami czy też płacić faktury VAT.'],
+   'Nasza firma, oferuje szeroki zakres usług ułatwiających pracę i zarządzanie klientami. Dzięki Bandalight możesz w prosty sposób zarządzać pracownikami czy też wystawiać faktury VAT.'],
   slideNumbers: 3,
   slideInterval: 6500
 };
